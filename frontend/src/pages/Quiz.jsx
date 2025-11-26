4cf23d12e9577fcaf99c4719b2b6fb414cf87da2
wesleycym <wesleycy@buffalo.edu>
2025-11-25 21:21:53 -0500

    Skeleton code for quiz and homepage

--- frontend/src/pages/Quiz.jsx
+++ frontend/src/pages/Quiz.jsx
@@ -1,2 +1,11 @@
 // Quiz page
 // Put quiz result into a cookie
+
+export default function Quiz() {
+  return (
+    <div>
+      <h1>Quiz Page</h1>
+      <a href="/">Back to Main</a>
+    </div>
+  );
+}
